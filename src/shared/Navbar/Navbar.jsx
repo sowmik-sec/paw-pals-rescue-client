842d407ac842d3d5d6ca2be8bfb65155971e47ba
sowmik-sec <sowmik.sec@gmail.com>
2024-09-15 21:56:08 +0600

    create responsive navbar

---
+++ src/shared/Navbar/Navbar.jsx
@@ -0,0 +1,53 @@
+import { NavLink } from "react-router-dom";
+
+function Navbar() {
+  const navItems = (
+    <>
+      <li>
+        <NavLink to="/pet-listing">Pet Listing</NavLink>
+      </li>
+      <li>
+        <NavLink to="/donation-campaigns">Donation Campaigns</NavLink>
+      </li>
+    </>
+  );
+  return (
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+          >
+            {navItems}
+          </ul>
+        </div>
+        <a className="btn btn-ghost text-xl">Paw Pals Rescue</a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">{navItems}</ul>
+      </div>
+      <div className="navbar-end">
+        <NavLink to="/login">Login</NavLink>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
