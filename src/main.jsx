23b013f88ed287b4a864dda059cb518ce085fe46
sowmik-sec <sowmik.sec@gmail.com>
2024-09-16 12:29:23 +0600

    use tanstack query provider in full app

--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,20 @@ import { RouterProvider } from "react-router-dom";
 import router from "./routes/router";
 import { HelmetProvider } from "react-helmet-async";
 import AuthProvider from "./providers/AuthProvider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AuthProvider>
-      <HelmetProvider>
-        <div className="max-w-screen-xl mx-auto">
-          <RouterProvider router={router} />
-        </div>
-      </HelmetProvider>
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <HelmetProvider>
+          <div className="max-w-screen-xl mx-auto">
+            <RouterProvider router={router} />
+          </div>
+        </HelmetProvider>
+      </AuthProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
