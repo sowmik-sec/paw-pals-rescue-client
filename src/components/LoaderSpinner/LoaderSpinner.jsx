ca119ea3eeabe5226f021af7141e29219d1b6930
sowmik-sec <sowmik.sec@gmail.com>
2024-09-16 12:29:33 +0600

    create custom loader

---
+++ src/components/LoaderSpinner/LoaderSpinner.jsx
@@ -0,0 +1,9 @@
+function LoaderSpinner() {
+  return (
+    <div className="flex justify-center text-success my-10">
+      <span className="loading loading-dots loading-lg"></span>
+    </div>
+  );
+}
+
+export default LoaderSpinner;
