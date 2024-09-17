7ac1d0fad2da5fa4bc8b26508c5fc2763a4cc885
sowmik-sec <sowmik.sec@gmail.com>
2024-09-17 21:14:48 +0600

    save uid of user in db

--- src/pages/auth/Signup/Signup.jsx
+++ src/pages/auth/Signup/Signup.jsx
@@ -26,6 +26,7 @@ function Signup() {
             const userInfo = {
               name: data.name,
               email: data.email,
+              uid: userCredential.user.uid,
             };
             axiosPublic.post("/users", userInfo).then((res) => {
               if (res.data.insertedId) {
