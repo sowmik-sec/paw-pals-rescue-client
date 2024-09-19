754897e2aac0cb2e57e39039f781937f09efb256
sowmik-sec <sowmik.sec@gmail.com>
2024-09-19 21:48:26 +0600

    create useAxiosSecure hook

---
+++ src/hooks/useAxiosSecure.jsx
@@ -0,0 +1,37 @@
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import useAuth from "./useAuth";
+
+const axiosSecure = axios.create({
+  baseURL: "http://localhost:5000",
+});
+function useAxiosSecure() {
+  const navigate = useNavigate();
+  const { logout } = useAuth();
+  axiosSecure.interceptors.request.use(
+    function (config) {
+      const token = localStorage.getItem("paw-pals-access-token");
+      config.headers.authorization = `Bearer ${token}`;
+      return config;
+    },
+    function (error) {
+      return Promise.reject(error);
+    }
+  );
+  axiosSecure.interceptors.response.use(
+    function (response) {
+      return response;
+    },
+    async function (error) {
+      const status = error.response.status;
+      if (status === 401 || status === 403) {
+        await logout();
+        navigate("/login");
+      }
+      return Promise.reject(error);
+    }
+  );
+  return axiosSecure;
+}
+
+export default useAxiosSecure;
