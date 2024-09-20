251cb0bb35b9cd2840fcff523caa3e2a077fd0fb
sowmik-sec <sowmik.sec@gmail.com>
2024-09-20 12:24:50 +0600

    create simple dashboard layout

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -1,9 +1,59 @@
-import { Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
+import { FaBars } from "react-icons/fa"; // Install react-icons: npm install react-icons
+import Navbar from "../shared/Navbar/Navbar";
 
 function DashboardLayout() {
   return (
     <div>
-      <Outlet />
+      <Navbar />
+      <div className="drawer lg:drawer-open">
+        <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+
+        {/* Toggle Button for Small Devices */}
+        <label
+          htmlFor="my-drawer-2"
+          className="btn btn-square btn-primary drawer-button fixed top-4 left-64 lg:hidden"
+        >
+          {/* Using a hamburger icon */}
+          <FaBars className="text-2xl" />
+        </label>
+
+        {/* Drawer Content (Main Page) */}
+        <div className="drawer-content flex flex-col items-center justify-center">
+          <Outlet />
+        </div>
+
+        {/* Sidebar */}
+        <div className="drawer-side">
+          <label
+            htmlFor="my-drawer-2"
+            aria-label="close sidebar"
+            className="drawer-overlay"
+          ></label>
+          <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+            <li>
+              <NavLink to="add-pet">Add a pet</NavLink>
+            </li>
+            <li>
+              <NavLink to="my-added-pets">My added pets</NavLink>
+            </li>
+            <li>
+              <NavLink to="adoption-requests">Adoption Request</NavLink>
+            </li>
+            <li>
+              <NavLink to="create-donation-campaign">
+                Create Donation Campaign
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="my-donation-campaign">My Donation Campaigns</NavLink>
+            </li>
+            <li>
+              <NavLink to="my-donation">My Donation </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
