4b04b0cf580fb1bf489cd6e31b81f8e21b2d66bc
sowmik-sec <sowmik.sec@gmail.com>
2024-09-24 12:36:12 +0600

    make a pet adopted

--- src/pages/Dashboard/MyAddedPets/MyAddedPets.jsx
+++ src/pages/Dashboard/MyAddedPets/MyAddedPets.jsx
@@ -3,10 +3,12 @@ import useAuth from "../../../hooks/useAuth";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import LoaderSpinner from "../../../components/LoaderSpinner/LoaderSpinner";
 import MyAddedPetsRow from "./MyAddedPetsRow";
+import Swal from "sweetalert2";
 
 function MyAddedPets() {
   const axiosSecure = useAxiosSecure();
   const { user } = useAuth();
+
   const {
     data: myPets,
     refetch,
@@ -18,6 +20,34 @@ function MyAddedPets() {
       return res.data;
     },
   });
+
+  const handleMarkAsAdopted = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes let it be adopted",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure
+          .patch(`/make-adopted/${id}`)
+          .then((res) => {
+            console.log(res);
+            Swal.fire({
+              title: "Thank you",
+              text: "You made it adopted.",
+              icon: "success",
+            });
+            refetch();
+          })
+          .catch((err) => console.error(err));
+      }
+    });
+  };
+
   if (isLoading) {
     return <LoaderSpinner />;
   }
@@ -41,7 +71,12 @@ function MyAddedPets() {
         </thead>
         <tbody>
           {myPets.map((pet, index) => (
-            <MyAddedPetsRow key={index} index={index} pet={pet} />
+            <MyAddedPetsRow
+              key={index}
+              index={index}
+              pet={pet}
+              handleMarkAsAdopted={handleMarkAsAdopted}
+            />
           ))}
         </tbody>
       </table>
