5ea0b62217938b8552c8eb5f0f20f1f790a6762f
sowmik-sec <sowmik.sec@gmail.com>
2024-09-27 21:17:48 +0600

    add text to dashboard

--- src/pages/Dashboard/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard/Dashboard.jsx
@@ -1,12 +1,21 @@
 import { Helmet } from "react-helmet-async";
+import useAuth from "../../../hooks/useAuth";
 
 function Dashboard() {
+  const { user } = useAuth();
   return (
     <div>
       <Helmet>
         <title>Paw Pals Rescue | Dashboard</title>
       </Helmet>
-      <h2>This is dashboard</h2>
+      <h2 className="text-4xl font-bold text-center">
+        Welcome{" "}
+        <span className="text-orange-500 text-center">{user.displayName}</span>{" "}
+        to dashboard
+      </h2>
+      <h3 className="text-3xl">
+        Navigate by the sidebar to necessary fields you have interest in.
+      </h3>
     </div>
   );
 }
