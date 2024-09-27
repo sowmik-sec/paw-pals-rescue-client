d0f3693376b8278657131edb195afd523e0c1107
sowmik-sec <sowmik.sec@gmail.com>
2024-09-27 21:25:12 +0600

    add vercel link

--- src/hooks/useAxiosPublic.jsx
+++ src/hooks/useAxiosPublic.jsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const axiosPublic = axios.create({ baseURL: "http://localhost:5000" });
+const axiosPublic = axios.create({
+  baseURL: "https://paw-pals-rescue-server.vercel.app",
+});
 
 function useAxiosPublic() {
   return axiosPublic;
