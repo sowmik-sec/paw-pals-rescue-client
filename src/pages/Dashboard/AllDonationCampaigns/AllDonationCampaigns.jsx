37ca6488e545663febe512bed12be1b97a38b11d
sowmik-sec <sowmik.sec@gmail.com>
2024-09-27 16:20:18 +0600

    create all donation campaign dashboard functionality

--- src/pages/Dashboard/AllDonationCampaigns/AllDonationCampaigns.jsx
+++ src/pages/Dashboard/AllDonationCampaigns/AllDonationCampaigns.jsx
@@ -1,7 +1,54 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import LoaderSpinner from "../../../components/LoaderSpinner/LoaderSpinner";
+import AllDonationCampaignsRow from "./AllDonationCampaignsRow";
+
 function AllDonationCampaigns() {
+  const axiosSecure = useAxiosSecure();
+  const { data: campaigns, isLoading } = useQuery({
+    queryKey: ["allDonationCampaigns"],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/all-donation-campaigns");
+      return res.data;
+    },
+  });
+  if (isLoading) {
+    return <LoaderSpinner />;
+  }
+  if (campaigns?.length === 0) {
+    return <h2 className="text-4xl">There is no active donation campaign</h2>;
+  }
   return (
     <div>
-      <h2 className="text-4xl">All Donation Campaigns</h2>
+      <h2 className="text-4xl">
+        Total Donation Campaigns: {campaigns?.length}
+      </h2>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>
+                <label>
+                  <h2 className="text-3xl">#</h2>
+                </label>
+              </th>
+              <th>Pet</th>
+              <th>Max Donation Limit</th>
+              <th>Donation Collected So far</th>
+            </tr>
+          </thead>
+          <tbody>
+            {campaigns.map((campaign, index) => (
+              <AllDonationCampaignsRow
+                key={campaign._id}
+                campaign={campaign}
+                index={index}
+              />
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
---
+++ src/pages/Dashboard/AllDonationCampaigns/AllDonationCampaignsRow.jsx
@@ -0,0 +1,37 @@
+import { useNavigate } from "react-router-dom";
+
+function AllDonationCampaignsRow({ campaign, index }) {
+  const { pet_name, pet_image, max_donation, totalAmount } = campaign;
+  const navigate = useNavigate();
+  return (
+    <tr>
+      <th>
+        <label>{index + 1}</label>
+      </th>
+      <td>
+        <div className="flex items-center gap-3">
+          <div className="avatar">
+            <div className="mask mask-squircle h-12 w-12">
+              <img src={pet_image} alt="pet image" />
+            </div>
+          </div>
+          <div>
+            <div className="font-bold">{pet_name}</div>
+          </div>
+        </div>
+      </td>
+      <td>{max_donation}</td>
+      <td>{totalAmount}</td>
+      <tr>
+        <button
+          className="btn btn-sm btn-info"
+          onClick={() => navigate(`/donation-details/${campaign._id}`)}
+        >
+          Details
+        </button>
+      </tr>
+    </tr>
+  );
+}
+
+export default AllDonationCampaignsRow;
